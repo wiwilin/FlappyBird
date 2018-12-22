3c42b392e0721f48fe8d56fc09925eb65a05634a
Wei Lin <katherine.linw@gmail.com>
2018-12-22 20:04:07 +0800

    Initial Commit

---
+++ FlappyBird22Tests/FlappyBird22Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FlappyBird22Tests.swift
+//  FlappyBird22Tests
+//
+//  Created by Wei Lin on 2018/12/22.
+//  Copyright © 2018 Wei Lin. All rights reserved.
+//
+
+import XCTest
+@testable import FlappyBird22
+
+class FlappyBird22Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
